e833becb3e89184d574a1fbd5b3c3396bfa5468a
unknown <zeeshanmehdi18@gmail.com>
2020-05-26 10:01:31 +0500

    changes

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ io.on("connection", function(socket) {
   });
 });
 
-http.listen(3000, function() {
+http.listen(process.env.PORT||3000, function() {
   console.log("listening to port:3000");
 });
